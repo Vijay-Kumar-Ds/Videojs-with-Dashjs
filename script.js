a382b7d5fd193d59caa85de5732f1b978cb59b64
Hung Nguyen <hung.nguyen@hotmail.com.vn>
2015-05-11 14:21:41 +0700

    update code for multi ads

--- script.js
+++ script.js
@@ -50,16 +50,16 @@
             className: 'vjs-btn-close',
             innerHTML: 'x'
         });
-        
+        /*
         var newDivTimer = videojs.createEl('div', {
             className: 'vjs-div-time',
-            innerHTML: 'Skip Ads in ' + (this.options_.advertisement.setAdvertisementTime)
+            innerHTML: 'Skip Ads in ' + (this.options_.advertisement.quantity.setAdvertisementTime)
         });
         
         this.newDivTimer_ = newDivTimer;
         
         newDiv.appendChild(this.newDivTimer_);
-        
+        */
         this.newDivClose_ = newDivClose;
         
         newDiv.appendChild(this.newDivClose_);
@@ -153,32 +153,6 @@
         var myComponent =  new videojs.containerDiv(this, options);
        
         
-        
-        
-        //countdown
-        function startTimer(secs){
-            timeInSecs = parseInt(secs - 1);
-            ticker = setInterval(tick,1000);   // every second
-        }
-
-        function tick() {
-            var secs = timeInSecs;
-            if (secs > 0) {
-                timeInSecs--;
-            } else {
-                clearInterval(ticker); // stop counting at zero
-            }
-            if (secs === 0) {
-                myComponent.newDivTimer_.innerText = 'Skip Ads';
-                myComponent.newDivTimer_.style.right = 23 + 'px';
-                myComponent.newDivClose_.style.opacity = 1;
-            } else {
-                myComponent.newDivTimer_.innerText = 'Skip Ads in ' + secs;
-            }
-        }    
-        
-       
-        
         //Get screen sizes
         var getWidth = this.width(this.offsetWidth, false);
         var getHeight = this.height(this.offsetHeight, false);
@@ -193,7 +167,7 @@
                 
                 var getCTime = Math.floor(this.cache_.currentTime);
                 
-                if (getCTime == myComponent.getTimeFromArray(i)) {
+                if (getCTime == myComponent.getTimeFromArray(i) && options.advertisement.optional === "random") {
                     
                     var myAds =  new videojs.containerDiv(this, options);
                     var myNewDiv = this.addChild(myAds);
@@ -207,13 +181,18 @@
 
                     var randomWidth = Math.floor(1 + Math.random() * (getSegmentWidth - 1));
                     var randomHeight = Math.floor(1 + Math.random() * (getHeight - 1));
-
+                    var randomWidthAds = Math.floor(getWidthAds/2 + Math.random() * (getWidthAds - getWidthAds/2));
+                    
                     if (getWidthAds == myAds.getNewWidth(this.options)) {
+                        myNewDiv.el_.style.width = randomWidthAds + 'px';
                         myNewDiv.el_.style.left = 1 + 'px';
                         myNewDiv.el_.style.top = Math.abs(randomHeight - (getHeightAds - 30)) + 'px';
+                        myNewDiv.newDivClose_.style.opacity = 1;
                     } else {
+                        myNewDiv.el_.style.width = randomWidthAds + 'px';
                         myNewDiv.el_.style.left = Math.abs(randomWidth) + 'px';
                         myNewDiv.el_.style.top = Math.abs(randomHeight - (getHeightAds - 30)) + 'px';
+                        myNewDiv.newDivClose_.style.opacity = 1;
                     }
 
                     this.one(myNewDiv.newDivClose_,'click', function() {
@@ -221,6 +200,27 @@
                     });
                     
                     i++;
+                } else if (getCTime == myComponent.getTimeFromArray(i) && options.advertisement.optional === "bottom") {
+                
+                    var myNewDiv = this.addChild(myComponent);
+                    
+                    myNewDiv.el_.style.width = 80 + '%';
+                    
+                    //Get screen size of ads
+                    var getWidthAds = myNewDiv.el_.offsetWidth;
+                    
+                    var getPositionWidth = (options.wideScreen.Width - getWidthAds) / 2;
+                    console.log(options.wideScreen.Height);
+                    myNewDiv.el_.style.left = getPositionWidth + 'px';
+                    myNewDiv.el_.style.bottom = 40 + 'px';
+                    myNewDiv.newDivClose_.style.opacity = 1;
+                    
+                    myNewDiv.contentEl_.innerHTML = options.advertisement.quantity[i].contentAds;
+                    
+                    this.one(myNewDiv.newDivClose_,'click', function() {
+                        this.removeChild(myComponent);
+                    });
+                    i++
                 }
                 
             });
